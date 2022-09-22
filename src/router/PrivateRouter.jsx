bc9c2bfe5e26d13561ee2b401bf3e49628ee968b
ahmetaric <ahmet.aric1965@gmail.com>
2022-09-22 18:36:05 +0300

    v-4

--- src/router/PrivateRouter.jsx
+++ src/router/PrivateRouter.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Navigate, Outlet } from 'react-router-dom';
-const user = true;
+const user = JSON.parse(sessionStorage.getItem("user"));
 
 const PrivateRouter = () => {
   return (
