da9c01b8861e60e13e9d381c796957f6b20b825a
ahmetaric <ahmet.aric1965@gmail.com>
2022-09-23 12:03:30 +0300

    v-5

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import NavbarStyle from "./Navbar.style"
+import React, { useState } from "react";
+import Nav, { Logo, MenuLink, Menu, Hamburger } from "./Navbar.style";
+import { Link } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false)
+    
   return (
-    <div>Navbar</div>
-  )
-}
+    <Nav justify="space-between" wrap="wrap">
+      <Logo to="/">
+        <i>{"<Ahmet/>"}</i>
+        <span>Recipe</span>
+      </Logo>
+      <Hamburger onClick={()=>setIsOpen(!isOpen)}>
+        <GiHamburgerMenu />
+      </Hamburger>
+      <Menu isOpen={isOpen} onClick={()=>setIsOpen(false)}>
+        <MenuLink to="/">Home</MenuLink>
+        <MenuLink to="about">About</MenuLink>
+        <MenuLink to="register">Register</MenuLink>
+        <MenuLink to="login" onClick={()=>sessionStorage.clear()}>Logout</MenuLink>
+      </Menu>
+    </Nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
