b069ca3659a39ed4aacb55f96b24a3bfbbff1d6a
ahmetaric <ahmet.aric1965@gmail.com>
2022-09-24 12:44:53 +0300

    v-10

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import AboutStyle from "./About.style"
+
+import { AboutContainer, InfoContainer, StyledImage } from "./About.style";
+
+import codingSvg from "../../assets/coding.svg";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <AboutContainer wrap="wrap">
+      <StyledImage src={codingSvg} />
+      <div>
+        <h1>
+          About Software Developer <span>Ahmet Ariç</span>
+        </h1>
+      </div>
+      <InfoContainer>
+        <h2>Hi, I'am Ahmet</h2>
+        <h3>I’m currently learning Full-Stack Development Languages.</h3>
+        <h4>I've already known JS, ReactJS,SQL, Python.</h4>
+        <h2>
+          <a href="mailto:ahmet.aric1965@gmail.com">Send email</a> :
+          ahmet.aric1965@gmail.com
+        </h2>
+      </InfoContainer>
+    </AboutContainer>
+  );
+};
 
-export default About
+export default About;
