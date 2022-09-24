9425d39b34809c9e845bd1714a89a3c91a8fe080
ahmetaric <ahmet.aric1965@gmail.com>
2022-09-24 11:52:12 +0300

    v-9

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { DetailContainer, DetailPart, HeaderContainer } from './Detail.style'
+import { useLocation } from 'react-router-dom'
+import dietSvg from "../../assets/diet.svg";
 
 const Detail = () => {
+  const {state} = useLocation();
+  console.log(state)
   return (
-    <div>Detail</div>
-  )
+    <DetailContainer>
+      <HeaderContainer>
+        <h1>{state.label}</h1>
+        <img src={dietSvg} alt="dietSvg" />
+      </HeaderContainer>
+      <DetailPart wrap="wrap">
+        
+      </DetailPart>
+    </DetailContainer>
+  );
 }
 
 export default Detail
